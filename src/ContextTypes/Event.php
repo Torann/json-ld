b0955b9e7497f61b227b4fd6b6109d04e9868807
Daniel Stainback <torann@gmail.com>
2019-05-08 11:50:27 -0400

    Update Event.php

--- src/ContextTypes/Event.php
+++ src/ContextTypes/Event.php
@@ -18,20 +18,20 @@ class Event extends Thing
         'location' => Place::class,
     ];
 
-	/**
-	 * Constructor. Merges extendedStructure up
-	 *
-	 * @param array $attributes
-	 * @param array $extendedStructure
-	 */
-	public function __construct(array $attributes, array $extendedStructure = [])
-	{
-		parent::__construct(
-			$attributes, array_merge($this->structure, $this->extendedStructure, $extendedStructure)
-		);
-	}
+    /**
+     * Constructor. Merges extendedStructure up
+     *
+     * @param array $attributes
+     * @param array $extendedStructure
+     */
+    public function __construct(array $attributes, array $extendedStructure = [])
+    {
+        parent::__construct(
+            $attributes, array_merge($this->structure, $this->extendedStructure, $extendedStructure)
+        );
+    }
 
-	/**
+    /**
      * Set offers attributes.
      *
      * @param  mixed $values
